d7c9d514839830bf00af168bb6eb3be6af1664b2
Colden Cullen <ColdenCullen@gmail.com>
2013-11-08 11:46:41 -0500

    Filtered table items to exclude header.

--- snippets.js
+++ snippets.js
@@ -1,1 +1,5 @@
-document.getElementById( 'ptifrmtgtframe' ).contentDocument.getElementsByClassName( "PSGROUPBOXWBO" );
+var tableItems = document.getElementById( 'ptifrmtgtframe' ).contentDocument.getElementsByClassName( "PSGROUPBOXWBO" );
+var classes = [];
+
+for( var ii = 1; ii < tableItems.length; ++ii )
+	classes.push( tableItems[ ii ] );
