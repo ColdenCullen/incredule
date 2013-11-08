eca81fe9e8fad24e644175fadc1c9ae37bf56b94
Colden Cullen <ColdenCullen@gmail.com>
2013-11-08 11:53:48 -0500

    Added linqjs url.

--- snippets.js
+++ snippets.js
@@ -3,3 +3,5 @@ var classes = [];
 
 for( var ii = 1; ii < tableItems.length; ++ii )
 	classes.push( tableItems[ ii ] );
+
+var linqjs = '//cdnjs.cloudflare.com/ajax/libs/linq.js/2.2.0.2/linq.min.js';
