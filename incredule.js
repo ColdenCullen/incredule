d7a7a9ad3022f2d5a521a4935e2fdc0b02545965
mpn3712 <mpn3712@rit.edu>
2013-11-15 13:06:35 -0500

    removed alerts

--- incredule.js
+++ incredule.js
@@ -1,20 +1,17 @@
 chrome.browserAction.onClicked.addListener(function () {
 	chrome.tabs.executeScript( null, { file: "linq.min.js" }, function() {
-		alert('stuffs');	
 		var jsonArray = [
 			{ "user": { "id": 100, "screen_name": "d_linq" }, "text": "to objects" },
 			{ "user": { "id": 130, "screen_name": "c_bill" }, "text": "g" },
 			{ "user": { "id": 155, "screen_name": "b_mskk" }, "text": "kabushiki kaisha" },
 			{ "user": { "id": 301, "screen_name": "a_xbox" }, "text": "halo reach" }
 		]
-		// ["b_mskk:kabushiki kaisha", "c_bill:g", "d_linq:to objects"]
 		var queryResult = Enumerable.From(jsonArray)
 			.Where(function (x) { return x.user.id < 200 })
 			.OrderBy(function (x) { return x.user.screen_name })
 			.Select(function (x) { return x.user.screen_name + ':' + x.text })
 			.ToArray();
 
-		alert( "queryResult" );
 	} );
 
 	alert( "SCRIPT THANG" );
