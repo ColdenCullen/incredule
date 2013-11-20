0cf9df7a09a3e7fb08006fd95e1b803211f3b2c2
unknown <cpc1668@OH1380-11.main.ad.rit.edu>
2013-11-20 11:54:14 -0500

    Added basic class parsing and message passing.

--- background.js
+++ background.js
@@ -1,34 +1,20 @@
 (function() {
-	var tabId;
-
-	// Execute our page-side script
-	chrome.tabs.executeScript( { file: "foreground.js" }, function() {
-		// Get the current tab
-		chrome.tabs.query( { active: true, currentWindow: true }, function( tabs ) {
-			tabId = tabs[0].id;
-		} );
-	} );
-
-	function sendMessage( request, callback ) {
-		chrome.tabs.sendMessage( tabId, request, function( response ) {
+	function sendMessage( tabid, request, callback ) {
+		chrome.tabs.sendMessage( tabid, request, function( response ) {
 			callback( response );
 		} );
 	}
 
 	// When you click the button...
 	chrome.browserAction.onClicked.addListener( function () {
-		sendMessage( "Hello", function( response ) {
-			console.log( "Response: " + response );
+		// Execute our page-side script
+		chrome.tabs.executeScript( { file: "foreground.js" } );
+
+		// Get the current tab
+		chrome.tabs.query( { active: true, currentWindow: true }, function( tabs ) {
+			sendMessage( tabs[0].id, "classes", function( response ) {
+				console.log( "Response: " + response );
+			} );
 		} );
-	});
-})();
-
-/*
-var tableItems = document.getElementById( 'ptifrmtgtframe' ).contentDocument.getElementsByClassName( "PSGROUPBOXWBO" );
-var classes = [];
-
-for( var ii = 1; ii < tableItems.length; ++ii )
-	classes.push( tableItems[ ii ] );
-
-alert( classes );
-*/
+	} );
+} )();
--- foreground.js
+++ foreground.js
@@ -1,9 +1,18 @@
+var Class = function( table ) {
+	this.name = table.rows[ 0 ].innerText;
+	console.log( this.name );
+};
+
 chrome.runtime.onMessage.addListener(
 function(request, sender, sendResponse) {
-	console.log( "Request received: " + request );
 
-	if( true )
-		sendResponse( "goodbye" );
+	var tableItems = document.getElementById( 'ptifrmtgtframe' ).contentDocument.getElementsByClassName( "PSGROUPBOXWBO" );
+	var classes = [];
+
+	for( var ii = 1; ii < tableItems.length; ++ii )
+		classes.push( new Class( tableItems[ ii ] ) );
+
+	sendResponse( classes );
 
 	return true;
-});
+} );
