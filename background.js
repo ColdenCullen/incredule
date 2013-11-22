30fc5756d8a9d281db2ac2cb7086fc5920f8a39e
Colden Cullen <ColdenCullen@gmail.com>
2013-11-22 11:50:33 -0500

    Did more class parsing, preparing for google calendar integration.

--- background.js
+++ background.js
@@ -1,8 +1,11 @@
 (function() {
-	function sendMessage( tabid, request, callback ) {
-		chrome.tabs.sendMessage( tabid, request, function( response ) {
-			callback( response );
-		} );
+	var gCalendarUrl = "https://www.googleapis.com/calendar/v3";
+
+	function Class( object ) {
+		object.startDate = new Date( object.startDate );
+		object.endDate = new Date( object.endDate );
+
+		return object;
 	}
 
 	// When you click the button...
@@ -12,8 +15,13 @@
 
 		// Get the current tab
 		chrome.tabs.query( { active: true, currentWindow: true }, function( tabs ) {
-			sendMessage( tabs[0].id, "classes", function( response ) {
-				console.log( "Response: " + response );
+			chrome.tabs.sendMessage( tabs[0].id, "classes", function( response ) {
+				var classes = JSON.parse( response );
+
+				for( var ii = 0; ii < classes.length; ++ii )
+					classes[ ii ] = new Class( classes[ ii ] );
+
+				console.log( classes[ 0 ].startDate );
 			} );
 		} );
 	} );
--- foreground.js
+++ foreground.js
@@ -1,18 +1,39 @@
 var Class = function( table ) {
+	function getElementByIdStart( id ) {
+		return table.querySelector( "span[id^=" + id + "]" ).innerText;
+	}
+
+	// Get name
 	this.name = table.rows[ 0 ].innerText;
-	console.log( this.name );
+
+	// Get start and end date
+	this.dateRange = getElementByIdStart( "MTG_DATES" );
+	this.startDate = new Date( this.dateRange.split( "-" )[0].trim() );
+	this.endDate = new Date( this.dateRange.split( "-" )[1].trim() );
+
+	// Get location
+	this.location = getElementByIdStart( "MTG_LOC" );
+
+	// Get days and times
+	this.daysAndTimes = getElementByIdStart( "MTG_SCHED" );
+	this.days = this.daysAndTimes.split( " " )[ 0 ].trim();
+	this.times = this.daysAndTimes.split( " " ).splice( 1 ).join( "" ).trim();
+	this.startTime = this.times.split( "-" )[ 0 ];
+	this.endTime = this.times.split( "-" )[ 1 ];
 };
 
 chrome.runtime.onMessage.addListener(
 function(request, sender, sendResponse) {
 
-	var tableItems = document.getElementById( 'ptifrmtgtframe' ).contentDocument.getElementsByClassName( "PSGROUPBOXWBO" );
-	var classes = [];
+	var tableItems = document.getElementById( 'ptifrmtgtframe' )
+							 .contentDocument
+							 .getElementsByClassName( "PSGROUPBOXWBO" ),
+		classes = [];
 
 	for( var ii = 1; ii < tableItems.length; ++ii )
 		classes.push( new Class( tableItems[ ii ] ) );
 
-	sendResponse( classes );
+	sendResponse( JSON.stringify( classes ) );
 
 	return true;
 } );
