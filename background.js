551de44e253e8668c288b445117a66a83821eecb
Colden Cullen <ColdenCullen@gmail.com>
2013-12-02 11:31:27 -0500

    Moved functions around, added basic google auth support.

--- background.js
+++ background.js
@@ -8,22 +8,36 @@ function load() {
 
 	//gapi.client.setApiKey( 'KEY' );
 
-	// When you click the button...
-	chrome.browserAction.onClicked.addListener( function () {
-		// Execute our page-side script
-		chrome.tabs.executeScript( { file: "foreground.js" } );
+	// Initialize authorization
+	gapi.auth.init( function() {
+		// When you click the button...
+		chrome.browserAction.onClicked.addListener( buttonClick );
+	} );
+}
 
+function buttonClick() {
+	// Execute our page-side script
+	chrome.tabs.executeScript( { file: "foreground.js" } );
+
+	var config = {
+		client_id: "",
+		scope: "https://www.googleapis.com/auth/calendar"
+	};
+
+	gapi.auth.authorize( config, function() {
 		// Get the current tab
 		chrome.tabs.query( { active: true, currentWindow: true }, function( tabs ) {
-			chrome.tabs.sendMessage( tabs[0].id, "classes", function( response ) {
-				var classes = JSON.parse( response );
-
-				for( var ii = 0; ii < classes.length; ++ii )
-					classes[ ii ] = new Class( classes[ ii ] );
-
-				console.log( classes[ 0 ] );
-				console.log( gapi );
-			} );
+			chrome.tabs.sendMessage( tabs[0].id, "classes", messageResponse );
 		} );
 	} );
+}
+
+function messageResponse( response ) {
+	var classes = JSON.parse( response );
+
+	for( var ii = 0; ii < classes.length; ++ii )
+		classes[ ii ] = new Class( classes[ ii ] );
+
+	console.log( classes[ 0 ] );
+	console.log( gapi );
 }
