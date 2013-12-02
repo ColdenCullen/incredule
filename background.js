df50fa21082c56fd6ddc40300556b9b0dc50babe
mpn3712 <mpn3712@rit.edu>
2013-12-02 12:21:20 -0500

    added config settings minus the key

--- background.js
+++ background.js
@@ -1,48 +1,43 @@
 function Class( object ) {
-	object.startDate = new Date( object.startDate );
-	object.endDate = new Date( object.endDate );
+    object.startDate = new Date( object.startDate );
+    object.endDate = new Date( object.endDate );
 
-	return object;
+    return object;
 }
 
 function load() {
-	//gapi.client.setApiKey( 'KEY' );
+    //gapi.client.setApiKey( 'KEY' );
 
-	// Initialize authorization
-	gapi.auth.init( function() {
-		// When you click the button...
-		chrome.browserAction.onClicked.addListener( buttonClick );
-	} );
+    // Initialize authorization
+    gapi.auth.init( function() {
+        // When you click the button...
+        chrome.browserAction.onClicked.addListener( buttonClick );
+    } );
 }
 
 function buttonClick() {
-	// Execute our page-side script
-	chrome.tabs.executeScript( { file: "foreground.js" } );
-
-	var config = {
-		client_id: config.client_id,
-		scope: "https://www.googleapis.com/auth/calendar",
-		immediate: true
-	};
-
-	gapi.auth.authorize( config, function( token ) {
-		// Get the current tab
-		chrome.tabs.query( { active: true, currentWindow: true }, function( tabs ) {
-			chrome.tabs.sendMessage( tabs[0].id, "classes", messageResponse );
-		} );
-	} );
+    // Execute our page-side script
+    chrome.tabs.executeScript( { file: "foreground.js" } );
+    console.log(config);
+    gapi.auth.authorize( config, function( token ) {
+        console.log('authorized');
+        // Get the current tab
+        chrome.tabs.query( { active: true, currentWindow: true }, function( tabs ) {
+            chrome.tabs.sendMessage( tabs[0].id, "classes", messageResponse );
+        } );
+    } );
 }
 
 function messageResponse( response ) {
-	var classes = JSON.parse( response );
+    var classes = JSON.parse( response );
 
-	for( var ii = 0; ii < classes.length; ++ii )
-		classes[ ii ] = new Class( classes[ ii ] );
+    for( var ii = 0; ii < classes.length; ++ii )
+        classes[ ii ] = new Class( classes[ ii ] );
 
-	console.log( classes[ 0 ] );
-	console.log( gapi.client );
+    console.log( classes[ 0 ] );
+    console.log( gapi.client );
 
-	gapi.client.load( 'calendar', 'v3', function() {
-		console.log( gapi.client );
-	} );
+    gapi.client.load( 'calendar', 'v3', function() {
+        console.log( gapi.client );
+    } );
 }
---
+++ config.js
@@ -0,0 +1,5 @@
+var config = {
+    client_id: "YOUR_KEY",
+    scope: "https://www.googleapis.com/auth/calendar",
+    immediate: true
+};
