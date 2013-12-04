7129cc98b21b2ebc4e120182b8f21f7f3b5ea8ab
Colden Cullen <ColdenCullen@gmail.com>
2013-12-04 11:21:18 -0500

    Narrowed permissions, added basic calendar filtering.

--- background.js
+++ background.js
@@ -1,58 +1,55 @@
 function Class( object ) {
-    object.startDate = new Date( object.startDate );
-    object.endDate = new Date( object.endDate );
+	object.startDate = new Date( object.startDate );
+	object.endDate = new Date( object.endDate );
 
-    return object;
+	return object;
 }
 
 function load() {
-    //gapi.client.setApiKey( 'KEY' );
-    // Initialize authorization
-    gapi.auth.init( function() {
-        // When you click the button...
-        document.getElementById('button').onclick = buttonClick;
-    } );
+	//gapi.client.setApiKey( 'KEY' );
+	// Initialize authorization
+	gapi.auth.init( function() {
+		// When you click the button...
+		document.getElementById('button').onclick = buttonClick;
+	} );
 }
 
 function buttonClick( event ) {
-    // Execute our page-side script
-    chrome.tabs.executeScript( { file: "foreground.js" } );
-    console.log(config);
-    gapi.client.setApiKey(config.api_key);
-    authorize();
+	// Execute our page-side script
+	chrome.tabs.executeScript( { file: "foreground.js" } );
+	gapi.client.setApiKey(config.api_key);
+	authorize();
 }
 
 function authorize() {
-    gapi.auth.authorize( config, function( token ) {
-        if( !token ) {
-            console.log(token);
-            config.immediate = false;
-            authorize();
-            config.immediate = true;
-        } else {
-            // Get the current tab
-            chrome.tabs.query( { active: true, currentWindow: true }, function(tabs ) {
-                chrome.tabs.sendMessage( tabs[0].id, "classes", messageResponse );
-            } );
-        }
-    } );
+	gapi.auth.authorize( config, function (token) {
+		if( !token ) {
+			config.immediate = false;
+			authorize();
+			config.immediate = true;
+		} else {
+			// Get the current tab
+			chrome.tabs.query( { active: true, currentWindow: true }, function(tabs ) {
+				chrome.tabs.sendMessage( tabs[0].id, "classes", messageResponse );
+			} );
+		}
+	} );
 }
 
 function messageResponse( response ) {
-    var classes = JSON.parse( response );
+	var classes = JSON.parse( response );
 
-    for( var ii = 0; ii < classes.length; ++ii )
-        classes[ ii ] = new Class( classes[ ii ] );
+	for( var ii = 0; ii < classes.length; ++ii )
+		classes[ ii ] = new Class( classes[ ii ] );
 
-    console.log( classes[ 0 ] );
+	gapi.client.load( 'calendar', 'v3', function() {
+		gapi.client.calendar.calendarList.list().execute( function (response) {
+			var calendars = response.items.filter( function (item) {
+				return item.accessRole == "owner" ||
+					   item.accessRole == "writer";
+			} );
 
-    gapi.client.load( 'calendar', 'v3', function() {
-        console.log( gapi.client.calendar );
-        console.log( gapi.client.calendar
-            .calendarList.list()
-            .execute(function (response) {
-                window.open('', 'Incredule');
-                console.log(response);
-            }) );
-    } );
+			console.log( calendars );
+		});
+	} );
 }
